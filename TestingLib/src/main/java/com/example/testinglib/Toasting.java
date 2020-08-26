a4c1401d15d198ece740db94b451f84db7e6277a
mohamed haimer <mohamed.haimer@adria-bt.com>
2020-08-26 09:46:43 +0100

    initial commit

--- TestingLib/src/main/java/com/example/testinglib/Toasting.java
+++ TestingLib/src/main/java/com/example/testinglib/Toasting.java
@@ -3,7 +3,7 @@ package com.example.testinglib;
 import android.content.Context;
 import android.widget.Toast;
 
-class Toasting {
+public class Toasting {
     public void show(Context context){
         Toast.makeText(context, "TEST 1", Toast.LENGTH_SHORT).show();
     }
