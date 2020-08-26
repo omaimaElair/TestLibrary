cfc7dafb79a6da6c0e06b1392be25d46e276799c
mohamed haimer <mohamed.haimer@adria-bt.com>
2020-08-26 11:35:46 +0100

    added Test class

---
+++ TestingLib/src/main/java/com/example/testinglib/Test.java
@@ -0,0 +1,13 @@
+package com.example.testinglib;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import es.dmoral.toasty.Toasty;
+
+class Test {
+    void show2(Context context){
+        Toast.makeText(context, "TEST 1", Toast.LENGTH_SHORT).show();
+        Toasty.error(context,"how you doing!",2000,true).show();
+    }
+}
