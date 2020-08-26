a4fcdfef8ee12cc53f75dbce526081f7e13207ed
mohamed haimer <mohamed.haimer@adria-bt.com>
2020-08-26 10:32:34 +0100

    added toasty

--- TestingLib/src/main/java/com/example/testinglib/Toasting.java
+++ TestingLib/src/main/java/com/example/testinglib/Toasting.java
@@ -3,8 +3,11 @@ package com.example.testinglib;
 import android.content.Context;
 import android.widget.Toast;
 
+import es.dmoral.toasty.Toasty;
+
 public class Toasting {
     public void show(Context context){
         Toast.makeText(context, "TEST 1", Toast.LENGTH_SHORT).show();
+        Toasty.error(context,"how you doing!",2000,true);
     }
 }
